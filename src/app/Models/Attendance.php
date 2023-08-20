1e0658fc8dd76a6fe4fca79ab4d0e7374189e3f2
pcspread <drummer0125vocal@sea.chukai.ne.jp>
2023-08-20 22:20:22 +0900

    I'm currently fixing and refactoring the personal part

--- src/app/Models/Attendance.php
+++ src/app/Models/Attendance.php
@@ -47,6 +47,17 @@ class Attendance extends Model
 
 
     /**
+     * XSS対策としてのエスケープ処理
+     * @param string $text エスケープ処理の対象データ
+     * @return string $result
+     */
+    public function xss($text)
+    {
+        return htmlspecialchars($text, ENT_QUOTES, 'UTF-8');
+    }
+
+
+    /**
      * datetime型のデータをdateに変換する
      * @param $element
      * @return string time
@@ -56,11 +67,76 @@ class Attendance extends Model
         return Carbon::parse($element)->format('H:i:s');
     }
 
+    /**
+     * 勤務開始時間の表示
+     * @param object $attendances レコードオブジェクト 
+     * @return array $arrayStart 勤務開始時間情報
+     */
+    public function displayStart($attendances)
+    {
+        // リターン用の配列を用意
+        $arrayStart = [];
+
+        // 勤務開始時間のデータが無い場合
+        if (empty($attendances[0])) {
+            $arrayStart[] = '(出勤情報無)';
+        }
+
+        // 勤務開始時間の件数計算用のデータを格納
+        if (count($attendances) > 1) {
+            $keyword = 1;
+        } else {
+            $keyword = '';
+        }
+
+        foreach ($attendances as $attendance) {
+            // 勤務開始時間データが複数ある場合
+            if ($keyword >= 1) {
+                // 件数を格納
+                $arrayStart[] = "[{$keyword}回目] ";
+                $keyword++;
+            }
+            // 勤務開始時間を格納
+            $arrayStart[] = Carbon::parse($attendance->start_at)->format('H:i:s');
+        }
+        return $arrayStart;
+    }
+
+    /**
+     * 勤務終了時間の表示
+     * @param object $attendances レコードオブジェクト
+     * @return array $arrayEnd 勤務終了時間情報
+     * 
+     */
+    public function displayEnd($attendances)
+    {
+        // 勤務終了時間格納用の配列を用意
+        $arrayEnd = [];
+
+        // 勤務終了時間の件数計算用のデータを格納
+        if (count($attendances) > 1) {
+            $num = 1;
+        } else {
+            $num = '';
+        }
+
+        foreach ($attendances as $attendance) {
+            // 件数が多い場合
+            if (!empty($num)) {
+                $arrayEnd[] = "[{$num}回目] ";
+                $num++;
+            }
+            // 勤務終了時間を格納
+            $arrayEnd[] = Carbon::parse($attendance->end_at)->format('H:i:s');
+        }
+        return $arrayEnd;
+    }
+
 
     /**
      * 休憩時間を計算する
-     * @param array $arrayBreak:休憩開始時間の配列
-     * @param array $arrayRestart:休憩終了時間の配列
+     * @param array $arrayBreak 休憩開始時間の配列
+     * @param array $arrayRestart 休憩終了時間の配列
      * @return string time:休憩時間
      */
     public function totalRes($arrayBreak, $arrayRestart)
@@ -101,9 +177,9 @@ class Attendance extends Model
 
     /**
      * 勤務時間を計算する
-     * @param string $start:勤務開始時間
-     * @param string $end:勤務終了時間
-     * @return string time:勤務時間
+     * @param string $start 勤務開始時間
+     * @param string $end 勤務終了時間
+     * @return string time 勤務時間
      */
     public function totalAtt($start, $end)
     {
