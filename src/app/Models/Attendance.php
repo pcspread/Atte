afadc6bb7e4421ef8952732fbf773c4505555cf3
pcspread <drummer0125vocal@sea.chukai.ne.jp>
2023-08-21 10:49:39 +0900

    To fixing the personal part will be completed soon.

--- src/app/Models/Attendance.php
+++ src/app/Models/Attendance.php
@@ -51,7 +51,7 @@ class Attendance extends Model
      * @param string $text エスケープ処理の対象データ
      * @return string $result
      */
-    public function xss($text)
+    public static function xss($text)
     {
         return htmlspecialchars($text, ENT_QUOTES, 'UTF-8');
     }
@@ -67,19 +67,34 @@ class Attendance extends Model
         return Carbon::parse($element)->format('H:i:s');
     }
 
+
     /**
-     * 勤務開始時間の表示
+     * 当日の日付の横にチェックマークを印字する
+     * @param object $day 日付データ
+     * @return string $result ✔マーク
+     */
+    public function displayTodayMark($day)
+    {
+        // 日付データが、サイト閲覧日と等しい場合
+        if ($day->toDateString() === Carbon::now()->toDateString()) {
+            return self::xss('✔');
+        }
+    }
+
+
+    /**
+     * 勤務開始時間の表示内容を作成する
      * @param object $attendances レコードオブジェクト 
      * @return array $arrayStart 勤務開始時間情報
      */
     public function displayStart($attendances)
     {
-        // リターン用の配列を用意
-        $arrayStart = [];
+        // 表示データ用の変数を初期化
+        $displayStart = '';
 
         // 勤務開始時間のデータが無い場合
         if (empty($attendances[0])) {
-            $arrayStart[] = '(出勤情報無)';
+            $displayStart .= self::xss('(出勤情報無)');
         }
 
         // 勤務開始時間の件数計算用のデータを格納
@@ -93,25 +108,27 @@ class Attendance extends Model
             // 勤務開始時間データが複数ある場合
             if ($keyword >= 1) {
                 // 件数を格納
-                $arrayStart[] = "[{$keyword}回目] ";
+                $displayStart .= self::xss("[{$keyword}回目] ");
                 $keyword++;
             }
             // 勤務開始時間を格納
-            $arrayStart[] = Carbon::parse($attendance->start_at)->format('H:i:s');
+            $displayStart .= self::xss(Carbon::parse($attendance->start_at)->format('H:i:s')) . '<br />';
         }
-        return $arrayStart;
+
+        return $displayStart;
     }
 
+
     /**
-     * 勤務終了時間の表示
+     * 勤務終了時間の表示内容を作成する
      * @param object $attendances レコードオブジェクト
      * @return array $arrayEnd 勤務終了時間情報
      * 
      */
     public function displayEnd($attendances)
     {
-        // 勤務終了時間格納用の配列を用意
-        $arrayEnd = [];
+        // 表示データ用の変数を初期化
+        $displayEnd = '';
 
         // 勤務終了時間の件数計算用のデータを格納
         if (count($attendances) > 1) {
@@ -123,71 +140,165 @@ class Attendance extends Model
         foreach ($attendances as $attendance) {
             // 件数が多い場合
             if (!empty($num)) {
-                $arrayEnd[] = "[{$num}回目] ";
+                $displayEnd .= self::xss("[{$num}回目] ");
                 $num++;
             }
             // 勤務終了時間を格納
-            $arrayEnd[] = Carbon::parse($attendance->end_at)->format('H:i:s');
+            $displayEnd .= self::xss(Carbon::parse($attendance->end_at)->format('H:i:s')) . '<br />';
         }
-        return $arrayEnd;
+
+        return $displayEnd;
     }
 
 
     /**
-     * 休憩時間を計算する
-     * @param array $arrayBreak 休憩開始時間の配列
-     * @param array $arrayRestart 休憩終了時間の配列
-     * @return string time:休憩時間
+     * 休憩合計時間を計算する
+     * @param object $attendances レコードオブジェクト
+     * @return array $displayRestTotal 休憩合計時間情報
      */
-    public function totalRes($arrayBreak, $arrayRestart)
+    public function displayRestTotal($attendances)
     {
-        // ベースの時間を設定
-        $base = Carbon::parse('00:00:00');
+        // 表示データ用の変数を初期化
+        $displayRestTotal = '';
 
-        // 返す結果を初期化
-        $result = '';
-        
-        // 休憩時間を全てベース時間に加算
-        for ($i = 0; $i < count($arrayBreak); $i++) {            
-            // 休憩開始時間を格納
-            $breakTime = Carbon::parse($arrayBreak[$i]);
+        // 休憩時間の回数計算用のデータを格納
+        if (count($attendances) > 1) {
+            $num = 1;
+        } else {
+            $num = '';
+        }
 
-            // 「休憩終了」が押されていない場合
-            if (is_null($arrayRestart[$i])) {
-                $result = '(休憩中)';
-            } else {
-                // 休憩終了時間を格納
-                $restartTime = Carbon::parse($arrayRestart[$i]);
-                // 休憩時間を計算
-                $total = $breakTime->diff($restartTime);
-                // 休憩時間を加算
-                $base = $base->add($total);
+        foreach ($attendances as $attendance) {
+            // レコードが複数ある場合
+            if (!empty($num)) {
+                $displayRestTotal .= self::xss("[{$num}回目] ");
+                $num++;
+            }
+
+            // 休憩時間を格納する配列を用意
+            $arrayBreak = [];
+            $arrayRestart = [];
+            
+            if (!empty($attendance->rests)) {
+                // 休憩開始と終了時間を格納
+                foreach ($attendance->rests as $item) {
+                    $arrayBreak[] = $item->break_at;
+                    $arrayRestart[] = $item->restart_at;
+                }
+
+                // ベースの時間を設定
+                $base = Carbon::parse('00:00:00');
+
+                // 休憩時間を格納する変数を初期化
+                $result = '';
+                
+                // 休憩時間を全てベース時間に加算
+                for ($i = 0; $i < count($arrayBreak); $i++) {            
+                    // 休憩開始時間を格納
+                    $breakTime = Carbon::parse($arrayBreak[$i]);
+
+                    // 「休憩終了」が押されていない場合
+                    if (is_null($arrayRestart[$i])) {
+                        $result = '(休憩中)';
+                    } else {
+                        // 休憩終了時間を格納
+                        $restartTime = Carbon::parse($arrayRestart[$i]);
+                        // 休憩時間を計算
+                        $total = $breakTime->diff($restartTime);
+                        // 休憩時間を加算
+                        $base = $base->add($total);
+                    }
+                }
+
+                // 休憩中でない場合は、休憩時間を格納
+                if ($result !== '(休憩中)') {
+                    $result = $base->format('H:i:s');
+                }        
+
+                // 「休憩終了」が押されていない場合は、00:00:00を返す
+                $displayRestTotal .= self::xss($result) . '<br />';
             }
         }
 
-        // 休憩中でない場合は、休憩時間を格納
-        if ($result !== '(休憩中)') {
-            $result = $base->format('H:i:s');
-        }        
-
-        // 「休憩終了」が押されていない場合は、00:00:00を返す
-        return $result;
+        return $displayRestTotal;
     }
 
 
     /**
-     * 勤務時間を計算する
-     * @param string $start 勤務開始時間
-     * @param string $end 勤務終了時間
-     * @return string time 勤務時間
+     * 勤務合計時間を計算する
+     * @param object $attendances レコードオブジェクト
+     * @return string $displayAttTotal 勤務合計時間情報
      */
-    public function totalAtt($start, $end)
+    public function displayAttTotal($attendances)
     {
-        $startTime = Carbon::parse($start);
-        $endTime = Carbon::parse($end);
-        // 勤務時間を計算
-        $diff = $startTime->diff($endTime);
-        return $diff->format('%H:%I:%S');
+        // 表示データ用の変数を初期化
+        $displayAttTotal = '';
+
+        // 勤務時間の回数計算用データを格納
+        if (count($attendances) > 1) {
+            $num = 1;
+        } else {
+            $num = '';
+        }
+        
+        foreach ($attendances as $attendance) {
+            // レコードが複数ある場合
+            if (!empty($num)) {
+                $displayAttTotal .= self::xss("[{$num}回目] ");
+                $num++;
+            }
+
+            if (empty($attendance->rests)) {
+                // 休憩時間レコードが無い場合
+                $displayAttTotal .= self::xss(Carbon::parse('00:00:00')->format('H:i:s')) . '<br />';
+            } else {
+                // 休憩時間レコードがある場合
+                $startTime = Carbon::parse($attendance->start_at);
+                $endTime = Carbon::parse($attendance->end_at);
+                // 勤務時間を計算
+                $diff = $startTime->diff($endTime);
+                $displayAttTotal .= self::xss($diff->format('%H:%I:%S')) . '<br />';
+            }
+        }
+
+        return $displayAttTotal;
+    }
+
+    /**
+     * 休憩時間詳細の表示内容を作成する
+     * @param object $attendances レコードオブジェクト
+     * @return string $displayRestDetail 休憩詳細情報
+     */
+    public function displayRestDetail($attendances)
+    {
+        // 表示データ用の変数を初期化
+        $displayRestDetail = '';
+        
+        // 出勤の回数計算用の変数用意
+        $count = 1;
+
+        foreach ($attendances as $attendance) {
+            // 出勤回数格納
+            $displayRestDetail .= self::xss("[{$count}回目]") . '<br />';
+
+            // 休憩情報レコードが無い場合
+            if (!$attendance->rests->first()) {
+            $displayRestDetail .= self::xss('休憩情報無') . '<br />';
+            }
+
+            // 休憩の回数計算用の変数用意
+            $count2 = 1;
+            
+            // 休憩時間を格納
+            foreach ($attendance->rests as $item) {
+                $detail = "({$count2}) {$item->break_at}～{$item->restart_at}";
+                $displayRestDetail .= '<p>' . self::xss($detail) . '</p>';
+                $count2++;
+            }
+            $count++;
+        }
+
+        return $displayRestDetail;
     }
 
 
