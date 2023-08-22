6d2f9b72c2458a461e3352f947d76b7c210cb260
pcspread <drummer0125vocal@sea.chukai.ne.jp>
2023-08-22 22:13:24 +0900

    This time, I did tests for this app.

--- src/tests/TestCase.php
+++ src/tests/TestCase.php
@@ -4,17 +4,17 @@ namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 // Seeder読込
-// use Database\Seeders\UserSeeder;
+use Database\Seeders\UserSeeder;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
 
-    // public function setUp(): void
-    // {
-    //     parent::setUp();
+    public function setUp(): void
+    {
+        parent::setUp();
 
-    //     // UserSeederを呼び出してデータ準備
-    //     $this->seed(UserSeeder::class);
-    // }
+        // UserSeederを呼び出してデータ準備
+        $this->seed(UserSeeder::class);
+    }
 }
